1a5a94885b23da889ac91dbcaf216330e7c1fffe
parimi naveen <pariminaveen429@gmail.com>
2012-12-07 23:27:15 +0000

    try to design view page and adding api to that

---
+++ cars/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cars::Application.config.secret_token = 'cd13c760e924528a8375e1213ede17c40685db8b9a40ea920e7c5893ce04834564ad57c1ddd4f49c02be881679d02ea013c727761765ad7b4445f7da160e2e94'
